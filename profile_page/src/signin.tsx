4cf0ad1bca9ea1b5da502ade4c6d4c7e0eef3393
vishnu <92919686+vexecute@users.noreply.github.com>
2023-07-17 22:24:39 +0530

    uploaded profile page

---
+++ profile_page/src/signin.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const signin: React.FC = () => {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      {/* Add your sign-in form and content here */}
+    </div>
+  );
+};
+
+export default signin;
